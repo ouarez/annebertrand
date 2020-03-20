37d9a95ddf44e3d739f1c68e588a77bc511f2edc
Martin Wares <waresmartin@gmail.com>
2020-03-19 20:10:32 -0400

    reworked media folder, removed /images/

--- static/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $(document).scroll(function () {
-    var $header = $("header");
-    $header.toggleClass('bb', $(this).scrollTop() > $nav.height());
-    $header.toggleClass('b--gray', $(this).scrollTop() > $nav.height());
-  });
-});
